76e1e96f0b3251239e643597269b9ffbd3df1dc8
x1XDanielX2xuser.name <daniel.delgado2000@hotmail.com>
2023-03-29 11:10:26 -0500

    cambios

--- resources/views/admin/plantilla.blade.php
+++ resources/views/admin/plantilla.blade.php
@@ -16,31 +16,31 @@
     <header>
         <nav class="container-fluid">
             <div class="row">
-                <div class="col-12 border">
-                    <h1 class="">CityBiker</h1>
+                <div class="col-8 py-2">
+                    <h1 class="ms-4"><a href="/admin" class="logo">CityBiker</a></h1>
+                </div>
+                <div class="col-4 espacio-login">
+                    <button type="submit" name="btnLogin" class="btn-primary">Iniciar Session</button>
                 </div>
             </div>
         </nav>
     </header>
 
-    <main clas="container-fluid">
-    <div class="row">
-        <div class="col-3 pe-5 opcionesAdmin">
-            <section>
-                <div class="row">
-                    <div class="col-12">
-                        <a href="/">Inicio</a>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <a href="/produtos">Productos</a>
-                    </div>
-                </div>
-            </section>
+    <main class="container-fluid">
+        <div class="row">
+        <aside class="barra_lateral col-4">
+            <nav class="menu">
+                <a href="/admin" class="py-3 mt-3">Inicio</a>
+                <a href="/productos" class="py-3">Productos</a>
+                <a href="/clientes" class="py-3">Clientes</a>
+                <a href="/categorias" class="py-3">Categorias</a>
+            </nav>
+        </aside>    
+        <div class="col-8">
+            @yield("contenido")
         </div>
     </div>
-    @yield("contenido")
-</main><footer></footer>
+</main>
+<footer></footer>
 </body>
 </html>
